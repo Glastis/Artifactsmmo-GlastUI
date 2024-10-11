a953fcc1a901032b5df322bf94c5e46274591e93
Glastis <glastis@glastis.com>
2024-10-12 01:01:42 +0200

    chore: removed dead code + useless comments

--- ui/GameMapView.py
+++ ui/GameMapView.py
@@ -37,22 +37,17 @@ class GameMapView(arcade.View):
         max_y = max(y_values)
         self.offset_x = -min_x
         self.offset_y = -min_y
-        self.max_y = max_y  # Stocker le maximum des Y
+        self.max_y = max_y
         for tile_data in map_data:
             tile = {}
             x = tile_data['x']
             y = tile_data['y']
             skin = tile_data.get('skin', 'forest_1')
-            # Obtenir la texture de la tuile
             tile_texture = self.textures['map'].get(skin, default_texture)
             tile['texture'] = tile_texture
             tile['data'] = tile_data
             tile['x'] = x
             tile['y'] = y
-            # is the tile already in the list?
-            for existing_tile in self.tiles:
-                if existing_tile['x'] == x and existing_tile['y'] == y:
-                    print (f"Tile {x}, {y} already exists")
             self.tiles.append(tile)
 
     def __load_textures(self):
@@ -76,31 +71,24 @@ class GameMapView(arcade.View):
         tile_width = texture.width
         tile_height = texture.height
 
-        # Inverser l'axe Y
         y_inverted = (self.max_y + self.offset_y) - y
 
         center_x = x * tile_width + tile_width / 2 + self.scroll_x
         center_y = y_inverted * tile_height + tile_height / 2 + self.scroll_y
 
-        # Dessiner la tuile
         arcade.draw_texture_rectangle(center_x, center_y, tile_width, tile_height, texture)
 
-        # Vérifier si la souris est sur cette tuile
         mouse_x = self.mouse_x
         mouse_y = self.mouse_y
 
-        # Calculer les limites de la tuile
         left = center_x - tile_width / 2
         right = center_x + tile_width / 2
         bottom = center_y - tile_height / 2
         top = center_y + tile_height / 2
 
-        # Vérifier si la position de la souris est dans les limites de la tuile
         if left <= mouse_x <= right and bottom <= mouse_y <= top:
-            # Dessiner un overlay semi-transparent
             arcade.draw_lrtb_rectangle_filled(left, right, top, bottom, (0, 0, 0, 100))
 
-        # Dessiner le contenu de la tuile s'il y en a
         if tile['data'] and tile['data'].get('content'):
             content_data = tile['data']['content']
             content_type = content_data['type']
@@ -127,7 +115,6 @@ class GameMapView(arcade.View):
             self.dragging = False
 
     def on_mouse_motion(self, x, y, dx, dy):
-        # Mettre à jour la position de la souris
         self.mouse_x = x
         self.mouse_y = y
 
